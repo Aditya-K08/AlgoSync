41ef7d6c51c73fe1bef2429e498484f332fda53d
Aditya <127967126+Aditya-K08@users.noreply.github.com>
2026-03-25 01:52:07 +0530

    Add solution for Four Divisors

--- leetcode/Four_Divisors.cpp
+++ leetcode/Four_Divisors.cpp
@@ -3,13 +3,13 @@ public:
     int sumFourDivisors(vector<int>& nums) {
         int ans=0;
         for(int x:nums){
-            int cnt=0,sum=0;
-            for(int i=1;i*i<=x;i++){
+            int cnt=2,sum=x+1;
+            for(int i=2;i*i<=x;i++){
                 if(x%i==0){
-                    cout<<i<<" ";
                     cnt++;
                     sum+=i;
-                    if(i*i!=x){
+                    cout<<x/i<<" "<<x<<endl;
+                    if(x/i!=i){
                         cnt++;
                         sum+=x/i;
                     }
