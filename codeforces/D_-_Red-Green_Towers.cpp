058454da3e41e4e09228739e8ececb2d109154fd
Aditya <127967126+Aditya-K08@users.noreply.github.com>
2026-03-24 14:13:53 +0530

    Add solution for D - Red-Green Towers

---
+++ codeforces/D_-_Red-Green_Towers.cpp
@@ -0,0 +1,52 @@
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+ 
+    int h=0;
+    while((h+1)*(h+2)/2<=r+g) h++;
+ 
+    vector<int> dp(r+1,0);
+    dp[0]=1;
+ 
+    for(int i=1;i<=h;i++){
+        for(int j=r;j>=i;j--){
+            dp[j]=(dp[j]+dp[j-i])%MOD;
+        }
+    }
+ 
+    int total=h*(h+1)/2;
+    int ans=0;
+ 
+    for(int j=0;j<=r;j++){
+        int green_used=total-j;
+        if(green_used<=g){
+            ans=(ans+dp[j])%MOD;
+        }
+    }
+ 
+    cout<<ans<<"\n";
+    return 0;
+}
