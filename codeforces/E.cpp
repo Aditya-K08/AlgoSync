ea05b9ed5c861e785a85d7fe6386f3a70ff8a199
Aditya <127967126+Aditya-K08@users.noreply.github.com>
2026-04-01 02:31:29 +0530

    Add solution for E

--- codeforces/E.cpp
+++ codeforces/E.cpp
@@ -3,21 +3,30 @@ using namespace std;
 void solve(){
     int n;
     cin>>n;
-    vector<int>arr(n);
+    vector<string>arr(n);
     for(auto &x:arr) cin>>x;
-    vector<int>ans(n,-1);
-    for(int i=0;i<n;i++){
-        int x=arr[i];
-        for(int j=i+1;j<n;j++){
-            int y=arr[j];
-            if((x&y)==0){
-                ans[i]=y;
-                ans[j]=x;
-                break;
-            }
+    vector<int>cnt(1<<24,0);
+    for(string s:arr){
+        int mask = (1<<(s[0]-'a')) | (1<<(s[1]-'a')) | (1<<(s[2]-'a'));
+        cnt[mask]++;
+    }
+    vector<int>dp=cnt;
+    for(int b=0; b<23; b++){
+        for(int mask=0; mask<(1<<22); mask++){
+            if(mask&(1<<b))
+                dp[mask]+=dp[mask^(1<<b)];
         }
-   }
-   for(int x:ans) cout<<x<<" ";
+    }
+    long long ans=0;
+    for(string s:arr){
+        int mask2=(1<<(s[0]-'a')) | (1<<(s[1]-'a')) | (1<<(s[2]-'a'));
+        for(int mask=0;mask<(1<<22);mask++){
+            //correct = n - sos[((1<<24)-1) ^ v]
+            int cor = n-dp[((1<<22)-1)^mask2];
+            ans^=cor*cor;
+        }
+    }
+    cout<<ans<<endl;
 }
 int main(){
     ios::sync_with_stdio(false);
