180245dc1bcbf23dba00d5eee601b71bd7b89887
Aditya <127967126+Aditya-K08@users.noreply.github.com>
2026-03-31 16:32:07 +0530

    Add solution for E

--- codeforces/E.cpp
+++ codeforces/E.cpp
@@ -3,37 +3,21 @@ using namespace std;
 void solve(){
     int n;
     cin>>n;
-    vector<vector<double>> arr(n, vector<double>(n));
-    for(int i=0;i<n;i++)
-        for(int j=0;j<n;j++)
-            cin>>arr[i][j];
-
-    vector<vector<double>>dp(1<<n,vector<double>(n,0));
-    for(int i=0;i<n;i++)
-        dp[1<<i][i]=1.0;
-
-    for(int mask=0; mask<(1<<n);mask++){
-        int k=__builtin_popcount(mask);
-        if(k<=1) continue;
-        double total=k*(k-1)/2.0;
-        for(int a=0;a<n;a++){
-            if(!((mask>>a)&1)) continue;
-            for(int b=a+1;b<n;b++){
-                if(!((mask>>b)&1)) continue;
-                dp[mask][a] += (1.0/total)*arr[a][b]*dp[mask^(1<<b)][a];
-                dp[mask][b] += (1.0/total)*arr[b][a]*dp[mask^(1<<a)][b];
-                for(int i=0;i<n;i++){
-                    if(!((mask>>i)&1)) continue;
-                    if(i==a || i==b) continue;
-                    dp[mask][i] += (1.0/total)*arr[a][b]*dp[mask^(1<<b)][i];
-                    dp[mask][i] += (1.0/total)*arr[b][a]*dp[mask^(1<<a)][i];
-                }
+    vector<int>arr(n);
+    for(auto &x:arr) cin>>x;
+    vector<int>ans(n,-1);
+    for(int i=0;i<n;i++){
+        int x=arr[i];
+        for(int j=i+1;j<n;j++){
+            int y=arr[j];
+            if((x&y)==0){
+                ans[i]=y;
+                ans[j]=x;
+                break;
             }
         }
-    }
-    for(int i=0;i<n;i++)
-        cout<<fixed<<setprecision(6)<<dp[(1<<n)-1][i]<<" ";
-    cout<<"\n";
+   }
+   for(int x:ans) cout<<x<<" ";
 }
 int main(){
     ios::sync_with_stdio(false);
