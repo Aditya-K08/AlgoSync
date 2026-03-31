ec8a31ada6a6537d2f4e57d112560642b841b956
Aditya <127967126+Aditya-K08@users.noreply.github.com>
2026-03-31 19:38:35 +0530

    Add solution for A - Antimedian Deletion

---
+++ codeforces/A_-_Antimedian_Deletion.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    for(int &x : a) cin >> x;
+
+    if(n == 1){
+        cout << "1\n\n";
+    } else {
+        for(int i = 0; i < n; i++){
+            cout << "2 ";
+        }
+        cout << "\n\n";
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while(t--) solve();
+
+    return 0;
+}
