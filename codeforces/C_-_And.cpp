4d07a5653aeaef402b98b3f141741386f54eec81
Aditya <127967126+Aditya-K08@users.noreply.github.com>
2026-04-01 22:37:03 +0530

    Add solution for C - And?

---
+++ codeforces/C_-_And.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    vector<int>arr(3);
+    cin>>arr[0]>>arr[1]>>arr[2];
+    sort(arr.begin(),arr.end());
+    cout<<((arr[0]^arr[1]^arr[2])-arr[1])<<endl;
+}
+int main(){
+    int t;
+    cin>>t;
+    while(t--) solve();
+    return 0;
+}
