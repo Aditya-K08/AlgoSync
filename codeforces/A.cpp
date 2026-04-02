7613896f9c4c6387b54e2ff30fd968bf359d1bef
Aditya <127967126+Aditya-K08@users.noreply.github.com>
2026-04-02 20:23:36 +0530

    Add solution for A

--- codeforces/A.cpp
+++ codeforces/A.cpp
@@ -1,33 +1,49 @@
 #include<bits/stdc++.h>
 using namespace std;
+
 void solve(){
-    int n;
-    cin>>n;
-    int left=INT_MAX,right=0;
-    vector<pair<int,int>>arr;
-    for(int i=1;i<=n;i++){
-        int l,r;
-        cin>>l>>r;
-        arr.push_back({l,r});
-        left=min(l,left);
-        right=max(r,right);
+    int n,m,k;
+    cin>>n>>m>>k;
+
+    vector<long long>arr(n);
+    for(auto &x:arr) cin>>x;
+
+    vector<int>l(m),r(m),d(m);
+    for(int i=0;i<m;i++){
+        cin>>l[i]>>r[i]>>d[i];
     }
-    vector<int>pref(right-left+2);
-    int prev=0;
-    for(int i=left;i<=right;i++){
-        int x=__builtin_popcount(i);
-        if(prev<x){
-            pref[i]=i;
-            prev=x;
-        }else{
-            pref[i]=pref[i-1];
-        }
-        //cout<<i<<" "<<pref[i]<<"\n";
+    vector<long long>op(m+2,0);
+    for(int i=0;i<k;i++){
+        int x,y;
+        cin>>x>>y;
+        op[x-1]++;
+        op[y]--;
     }
-    for(auto [l,r]:arr){
-        cout<<pref[r]<<endl;
+
+    for(int i=1;i<m;i++){
+        op[i]+=op[i-1];
     }
+
+    vector<long long>diff(n+2,0);
+
+    for(int i=0;i<m;i++){
+        long long times=op[i];
+        if(times==0) continue;
+
+        diff[l[i]-1]+=times*d[i];
+        diff[r[i]]-=times*d[i];
+    }
+
+    for(int i=1;i<n;i++){
+        diff[i]+=diff[i-1];
+    }
+
+    for(int i=0;i<n;i++){
+        cout<<arr[i]+diff[i]<<" ";
+    }
+    cout<<endl;
 }
+
 int main(){
     solve();
     return 0;
