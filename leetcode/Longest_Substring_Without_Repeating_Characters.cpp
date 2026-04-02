20703bd36bb9de9e1324a1429806049ffed2e263
Aditya <127967126+Aditya-K08@users.noreply.github.com>
2026-04-02 22:50:45 +0530

    Add solution for Longest Substring Without Repeating Characters

--- leetcode/Longest_Substring_Without_Repeating_Characters.cpp
+++ leetcode/Longest_Substring_Without_Repeating_Characters.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int n = s.size();
-        int i=0,j=0,ans=0;
+        int n=s.size();
+        int i=0;
+        int j=0;
+        int ans=0;
         unordered_map<char,int>mp;
-        for(int i=0;i<n;i++){
-            mp[s[i]]++;
-            while(mp[s[i]]>1){
+        while(i<n){
+            while(mp[s[i]]==1){
                 mp[s[j]]--;
-                j++;
+                if(mp[s[j]]==0) mp.erase(s[j]);
+                j++;   
             }
             ans=max(ans,i-j+1);
+            mp[s[i]]++;
+            i++;
         }
         return ans;
     }
