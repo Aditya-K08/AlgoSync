201c38aa8122b082db2da654b2429758b939ba80
Aditya <127967126+Aditya-K08@users.noreply.github.com>
2026-04-04 18:50:50 +0530

    Add solution for A - Submission Bait II

---
+++ codeforces/A_-_Submission_Bait_II.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        cout<<n+i<<" ";
+    }
+    cout<<"\n";
+}
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--) solve();
+}
