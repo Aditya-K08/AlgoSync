b2a471f7171da23380cca84f96f2103bdb8f7201
Aditya <127967126+Aditya-K08@users.noreply.github.com>
2026-04-04 20:49:01 +0530

    Add solution for E - The 67th XOR Problem

---
+++ codeforces/E_-_The_67th_XOR_Problem.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+
+vector<int>primes;
+vector<bool>is_prime(200005,true);
+void seive(){
+    is_prime[0]=is_prime[1]=false;
+    for (int i=2;i*i<=200005;i++) {
+        if (is_prime[i]){
+            for (int j=i*i;j<=200005;j+=i)
+                is_prime[j]=false;
+        }
+    }
+    for(int i=0;i<=200005;i++){
+        if(is_prime[i]) primes.push_back(i);
+    }
+}
+
+void solve(){
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    for(auto &x:arr){
+        cin>>x;
+    }
+    int ans=0;
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            ans=max(ans,arr[i]^arr[j]);
+        }
+    }
+    cout<<ans<<endl;
+}
+
+signed main(){
+    int t;
+    cin>>t;
+    //seive();
+    while(t--) solve();
+}
