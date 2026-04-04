9b6ac3fae1d38d3adc75e45181c2c4647e1e0ee2
Aditya <127967126+Aditya-K08@users.noreply.github.com>
2026-04-04 14:52:34 +0530

    Add solution for Number of Recent Calls

---
+++ leetcode/Number_of_Recent_Calls.cpp
@@ -0,0 +1,22 @@
+class RecentCounter {
+public:
+    queue<int>q;
+    RecentCounter() {
+        
+    }
+    
+    int ping(int t) {
+        q.push(t);
+        int left=t-3000;
+        while(q.front()<left){
+            q.pop();
+        }
+        return q.size();
+    }
+};
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter* obj = new RecentCounter();
+ * int param_1 = obj->ping(t);
+ */
