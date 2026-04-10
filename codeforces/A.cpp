a7c94b6e6e72216a0c132db91a029381597834de
Aditya <127967126+Aditya-K08@users.noreply.github.com>
2026-04-10 22:30:53 +0530

    Add solution for A

--- codeforces/A.cpp
+++ codeforces/A.cpp
@@ -2,15 +2,16 @@
 using namespace std;
 
 void solve(){
-    int n;
-    cin>>n;
-    vector<int>arr(n);
-    for(auto &x:arr) cin>>x;
-    vector<int>pos(n+1);
-    for(int i=0;i<n;i++){
-        pos[arr[i]]=i+1;
+    string s;
+    cin>>s;
+    int n=s.size();
+    int fours=count(s.begin(),s.end(),'4');
+    int sevens=count(s.begin(),s.end(),'7');
+    if(fours+sevens==n){
+        cout<<"YES"<<endl;
+    }else{
+        cout<<"NO"<<endl;
     }
-    for(int i=1;i<=n;i++) cout<<pos[i]<<" ";
 }
 
 int main(){
