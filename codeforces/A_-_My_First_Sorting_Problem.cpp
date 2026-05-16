00914745fffccede888f37d4d02e4bd97451a9e8
Aditya <127967126+Aditya-K08@users.noreply.github.com>
2026-05-17 03:02:58 +0530

    Add solution for A - My First Sorting Problem

---
+++ codeforces/A_-_My_First_Sorting_Problem.cpp
@@ -0,0 +1,19 @@
+// Source: https://usaco.guide/general/io
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int a,b;
+    cin>>a>>b;
+    cout<<min(a,b)<<" "<<max(a,b)<<"\n";
+}
+
+int main() {
+	int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
