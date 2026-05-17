e7507d28084cbeac8a195a38f7f2effa3a8dc31a
Aditya <127967126+Aditya-K08@users.noreply.github.com>
2026-05-17 13:31:28 +0530

    Add solution for A - Minimize!

---
+++ codeforces/A_-_Minimize!.cpp
@@ -0,0 +1,20 @@
+// Source: https://usaco.guide/general/io
+
+#include <bits/stdc++.h>
+#include <unordered_map>
+using namespace std;
+
+void solve(){
+    int a,b;
+    cin>>a>>b;
+    cout<<(b-a)<<"\n";
+}
+
+int main() {
+	int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
